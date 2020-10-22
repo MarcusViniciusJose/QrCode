9f0561876b428cf7ec81c90b17dcaaa5c363e2b4
MarcusVinicius <marcus.jose@etec.sp.gov.br>
2020-10-22 14:33:51 -0300

    Leitor

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    QRScanner,
+    ScreenOrientation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD7uAuDTeOFdY8WXEuIl5O_nzyXWfLihKg",
+    authDomain: "leitorqrcodefireb.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefireb.firebaseio.com",
+    projectId: "leitorqrcodefireb",
+    storageBucket: "leitorqrcodefireb.appspot.com",
+    messagingSenderId: "113316288586",
+    appId: "1:113316288586:web:b16b01317772e0a89beeb3",
+    measurementId: "G-BMD2DX5MNK"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
